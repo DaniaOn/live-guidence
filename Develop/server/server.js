5e8b6d96bf4f760ed5fe1733a671712b837ac9e3
danicaon <danica_escot@hotmail.com>
2023-04-16 09:10:52 -0500

    adding Apollo and other dev to server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const { ApolloServer } = require("apollo-server-express");//Apollo server
+const db = require('./config/connection');//db conections
+const routes = require('./routes');//db conections
 
+//express server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//Apollo server to pass to schema
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+server.applyMiddleware({ app });
+
+//middleware parse
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -16,6 +28,14 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+// get all client file
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/"));
+});
+
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => 
+  console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log('Use GraphQL at http://127.0.0.1:${PORT}${server.graphqlPath}')
+
 });
